da51d69481eff04735ede2d1f6b14c6c077513f9
Sri Balaji Gopinath <balaji.gopinath@sris-mbp.lan>
2024-06-07 16:49:15 +0530

    Cleaned up App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,24 +6,6 @@ import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import Flashcards from './components/flashcards/Flashcards';
 
-const flashcardsData = [
-  {
-    title: `परपतेद्द्यौः सनक्षत्रा पृथिवी श्कली भवेत |` + '\n' +
-    `शैत्यमग्निः इयान्न अहं त्यजेयं रघुनन्दनम् ||`,
-    content: `Transliteration (IAST):
-    prapateddyauḥ sanakṣatrā pṛthivī śakalī bhavet |
-    śaityam agniḥ iyān na ahaṃ tyajeyaṃ raghunandanam ||
-    
-    Translation:
-    Even if the sky falls along with the stars, and the Earth shatters into pieces,
-    And fire loses its heat, I will never abandon Raghunandana (Lord Rama).
-    
-    This verse expresses unwavering devotion and loyalty, highlighting that no matter how extreme the circumstances, the speaker's dedication to Lord Rama will remain steadfast.`,
-    tags: ['Ramayana']
-  }
-  // Add more flashcards as needed
-];
-
 function App() {
   return (
     <div className="main-container">
@@ -40,7 +22,7 @@ function App() {
         </TabPanel>
 
         <TabPanel>
-          <Flashcards flashcards={flashcardsData} />
+          <Flashcards />
         </TabPanel>
 
       </Tabs>
