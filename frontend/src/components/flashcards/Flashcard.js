689c07cae56697441acbe0aa6bc6bab5e11ed41b
Sri Balaji Gopinath <balaji.gopinath@sris-mbp.lan>
2025-05-01 11:33:13 +0530

    Adding a delete icon

--- frontend/src/components/flashcards/Flashcard.js
+++ frontend/src/components/flashcards/Flashcard.js
@@ -23,7 +23,11 @@ const Flashcard = ({ flashcard, onDelete }) => {
       <div onClick={handleOpenModal} className="flashcard-summary">
         <div className="flashcard-header">
           <h3>{flashcard.title}</h3>
-          <button onClick={handleDelete} className="delete-button">Delete</button>
+          <button onClick={handleDelete} className="delete-button" title="Delete flashcard">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" fill="currentColor"/>
+            </svg>
+          </button>
         </div>
         {flashcard.description && (
           <p className="description">{flashcard.description}</p>
