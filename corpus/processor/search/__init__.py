d28583bc74586b100e087ef88179e4200da60cca
Balaji <balaji@mac.lan>
2025-12-12 14:01:24 +0530

    Removed redundant files

--- corpus/processor/search/__init__.py
+++ corpus/processor/search/__init__.py
@@ -4,8 +4,7 @@ Search package for vector search functionality.
 
 from processor.search.vector_search import (
     search_semantic,
-    check_vector_search_support,
-    create_vector_search_index
+    check_vector_search_support
 )
 
-__all__ = ['search_semantic', 'check_vector_search_support', 'create_vector_search_index']
+__all__ = ['search_semantic', 'check_vector_search_support']
